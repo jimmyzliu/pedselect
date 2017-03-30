fdce6325357ca3547c1adc8debe353416a70bfba
jimmyzliu <jimmy.z.liu@gmail.com>
2017-03-30 11:56:40 -0400

    add pdf doc

--- pedselect.py
+++ pedselect.py
@@ -1,4 +1,32 @@
-# pedselect
+# Copyright (c) 2017, New York Genome Center
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+# Pedselect v0.1
+# March 30 2017
 
 # select most informative individuals to sequence from a pedigree to maximise coverage (and power) of phenotyped individuals
 # single input file: "IID FATHER MOTHER SEX GENO DNA PHENO"
@@ -179,7 +207,8 @@ def main():
 				best_score = sum(temp_scores)
 				best_i = i
 			# print str(sum(temp_scores)) + " " + temp_ind
-		print "Adding " + pheno_no_geno[best_i] + " (" + str(best_score) + ") to list"
+		#print "Adding " + pheno_no_geno[best_i] + " (" + str(best_score) + ") to list"
+		print pheno_no_geno[best_i] + " (score of remaining individuals = " + str(best_score) + ")"
 		geno_inds.append(pheno_no_geno[best_i])
 		picked.append(pheno_no_geno[best_i])
 	
@@ -201,7 +230,8 @@ def main():
 		if best_score == prev_best_score:
 			break
 		picked.append(best_ind)
-		print "Adding " + best_ind + " (" + str(best_score) + ") to list"
+		#print "Adding " + best_ind + " (" + str(best_score) + ") to list"
+		print best_ind + " (score of remaining individuals = " + str(best_score) + ")"
 		prev_best_score = best_score
 
 if __name__ == "__main__":
